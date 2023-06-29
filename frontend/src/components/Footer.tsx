c5ec73f5cbd85ae1648d7f5ea730a21861a00144
Raekwon Adams-Rodgers <adamsrodgers@etsu.edu>
2023-06-29 01:42:21 -0400

    finishing touches on footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, } from 'react-bootstrap';
 import '../css/Footer.css';
 import facebook from "../Images/facebook.svg";
 import instagram from "../Images/instagram.svg";
@@ -10,32 +10,34 @@ function Footer()
 
         <>
             <footer>
-                <Container className="text-center">
+                <Container className="text-center big">
                     <div className="row">
                         <div className="col-sm">
                             <ul className="list">
-                                <li className="listel"><a href="/Home" className="link">Home</a></li>
-                                <li className="listel"><a href="/Products" className="link">Products</a></li>
-                                <li className="listel"><a href="/About" className="link">About</a></li>
+                                <li className="listel"><p className="hov"><a href="/Home" className="link ">Home</a></p></li>
+                                <li className="listel"><p><a href="/Products" className="link">Products</a></p></li>
+                                <li className="listel"><p><a href="/About" className="link">About</a></p></li>
                             </ul>  
                         </div>
                         <div className="col-sm">
-                            column 2
+                            <p className="center">column 2</p>
+                             
                         </div>
                         <div className="col-sm">
-                            <div> Follow us on social media</div>
+                            <div> 
+                                <p className="down">Follow us on social media</p>
                             <div className="images">
-                                <img src={facebook} className="icon"/>
-                                <img src={instagram} className="icon"/>
+                                <a href="/facebook" className="icon"><img src={facebook}  width="40" height="40"/></a>
+                                <a href="/insta" className="icon"><img src={instagram}  width="40" height="40"/></a>
                             </div>
-                            
+                            </div>
                         </div>
                     </div>
                 </Container>
                 <Container>
                     <Row>
-                        <Col className="text-center ">
-                            copyright &copy; bububeckys sauces
+                        <Col className="text-center py-3">
+                           <p>copyright &copy;2023 bububeckys sauces</p> 
                         </Col>
                     </Row>
                 </Container>
